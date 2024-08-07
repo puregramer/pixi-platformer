2e74479c26a66666e0747a77c09c204546b87bc4
DongHun <ldh@wjtb.net>
2024-08-07 15:28:58 +0900

    - egg turret

--- src/components/enemy/eggTurret.ts
+++ src/components/enemy/eggTurret.ts
@@ -1,10 +1,56 @@
 import { Container } from 'pixi.js';
+import SpritesheetAnimation from '../SpritesheetAnimation';
+import { AnimState } from '../Player';
 
 export class EggTurret extends Container {
+    anim: SpritesheetAnimation;
+    currentState: AnimState | null = null;
+
+    static animStates: Record<string, AnimState> = {
+        idle: {
+            anim: "Idle",
+            loop: true,
+            speed: 0.13,
+        },
+        shoot: {
+            anim: "Shoot",
+            loop: false,
+            speed: 0.2,
+        },
+    };
+
+    config = {
+        speed: 5,
+        turnDuration: 0,
+        decelerateDuration: 0.2,
+        scale: 0.7,
+        shoot: {
+            duration: 0.2,
+            ease: "sine",
+        }
+    };
+
+    state = {
+        shooting: false,
+        velocity: {
+            x: 1,
+            y: 0,
+        },
+    };
 
     constructor() {
         super();
+        this.anim = new SpritesheetAnimation("enemy/egg-turret");
 
+        this.addChild(this.anim);
+
+        this.setState(EggTurret.animStates.idle);
+
+    }
+
+    setState(state: AnimState) {
+        this.currentState = state;
+        return this.anim.play(state);
     }
 
 }
